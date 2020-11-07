7d451cd67588763b9ad1fde98f863c72364cf5f2
DO-DRock <68431368+DO-DRock@users.noreply.github.com>
2020-11-06 22:04:00 -0700

    Updated to remove deprecation errors
    Removed "${ and }" to clean up warnings

--- main.tf
+++ main.tf
@@ -1,49 +1,49 @@
 locals {
   settings_windows = {
-    script   = "${compact(concat(list(var.command), split("\n", var.script)))}"
-    fileUris = "${var.file_uris}"
+    script   = compact(concat(list(var.command), split("\n", var.script)))
+    fileUris = var.file_uris
   }
 
   settings_linux = {
-    commandToExecute = "${var.command}"
-    fileUris         = "${var.file_uris}"
-    script           = "${base64encode(var.script)}"
+    commandToExecute = var.command
+    fileUris         = var.file_uris
+    script           = base64encode(var.script)
   }
 }
 
 data "azurerm_resource_group" "main" {
-  name = "${var.resource_group_name}"
+  name = var.resource_group_name
 }
 
 data "azurerm_virtual_machine" "main" {
-  name                = "${var.virtual_machine_name}"
-  resource_group_name = "${data.azurerm_resource_group.main.name}"
+  name                = var.virtual_machine_name
+  resource_group_name = data.azurerm_resource_group.main.name
 }
 
 resource "azurerm_virtual_machine_extension" "linux" {
-  count                      = "${lower(var.os_type) == "linux" ? 1 : 0}"
-  name                       = "${var.virtual_machine_name}-run-command"
-  location                   = "${data.azurerm_resource_group.main.location}"
-  resource_group_name        = "${data.azurerm_resource_group.main.name}"
-  virtual_machine_name       = "${data.azurerm_virtual_machine.main.name}"
-  publisher                  = "Microsoft.CPlat.Core"
-  type                       = "RunCommandLinux"
+  count                      = lower(var.os_type) == "linux" ? 1 : 0
+  name                       = "var.virtual_machine_name-run-command"
+  location                   = data.azurerm_resource_group.main.location
+  resource_group_name        = data.azurerm_resource_group.main.name
+  virtual_machine_name       = data.azurerm_virtual_machine.main.name
+  publisher                  = Microsoft.CPlat.Core"
+  type                       = RunCommandLinux"
   type_handler_version       = "1.0"
   auto_upgrade_minor_version = true
-  protected_settings         = "${jsonencode(local.settings_linux)}"
-  tags                       = "${var.tags}"
+  protected_settings         = jsonencode(local.settings_linux)
+  tags                       = var.tags
 }
 
 resource "azurerm_virtual_machine_extension" "windows" {
-  count                      = "${lower(var.os_type) == "windows" ? 1 : 0}"
-  name                       = "${var.virtual_machine_name}-run-command"
-  location                   = "${data.azurerm_resource_group.main.location}"
-  resource_group_name        = "${data.azurerm_resource_group.main.name}"
-  virtual_machine_name       = "${data.azurerm_virtual_machine.main.name}"
+  count                      = lower(var.os_type) == "windows" ? 1 : 0
+  name                       = "var.virtual_machine_name-run-command"
+  location                   = data.azurerm_resource_group.main.location
+  resource_group_name        = data.azurerm_resource_group.main.name
+  virtual_machine_name       = data.azurerm_virtual_machine.main.name
   publisher                  = "Microsoft.CPlat.Core"
   type                       = "RunCommandWindows"
   type_handler_version       = "1.1"
   auto_upgrade_minor_version = true
-  settings                   = "${jsonencode(local.settings_windows)}"
-  tags                       = "${var.tags}"
+  settings                   = jsonencode(local.settings_windows)
+  tags                       = {var.tags
 }
